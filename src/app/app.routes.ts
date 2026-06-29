aaa9475e89e86bb7ec55726844fdbd283687d04d
Castro <jamillymilly10@hotmail.com>
2026-06-28 21:12:07 -0300

    feat: initial frontend structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./presentation/views/auth/login/login.component').then(m => m.LoginComponent) },
+  { path: '', loadComponent: () => import('./presentation/views/cash-flow/cash-flow.component').then(m => m.CashFlowComponent) }
+];
---
+++ src/app/infrastructure/api/HttpClientWrapper.ts
@@ -0,0 +1,3 @@
+export class HttpClientWrapper {
+  // centraliza chamadas
+}
